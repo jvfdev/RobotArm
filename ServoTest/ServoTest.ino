15fbb4055685f533a94fd67a44a65295d2f3c042
jvfdev <38409266+jvfdev@users.noreply.github.com>
2018-09-02 11:12:50 -0500

    Added multi-servo control

--- ServoTest/ServoTest.ino
+++ ServoTest/ServoTest.ino
@@ -1,19 +1,50 @@
-// This script tests communciation between the PC and the arduino, as well as the function of the servo.
+String str;
 
 #include <Servo.h>
 
-Servo J0;
+Servo J0, J1, J2, J3, J4;
 
 void setup() {
   // put your setup code here, to run once:
+
+  J0.attach(13);
+  J1.attach(12);
+  J2.attach(11);
+  J3.attach(10);
+  J4.attach(9);
   Serial.begin(9600);
-  J0.attach(9);
+  Serial.println("Connection Established");
+
+  J0.writeMicroseconds(1475);
+  J1.writeMicroseconds(1900);
+  J2.writeMicroseconds(1800);
+  J3.writeMicroseconds(1000);
+ J4.writeMicroseconds(1900);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  while(Serial.available() > 0){
-    int ms = Serial.parseInt();
-    J0.writeMicroseconds(ms);
-  }
+  if (Serial.available() > 0) {
+    char serialData = Serial.read();
+    if (serialData == ',') {
+      //Serial.print("foo");
+      int n = str.toInt();
+      if (str.indexOf('a') > 0) J0.writeMicroseconds(n);
+      if (str.indexOf('b') > 0) J1.writeMicroseconds(n);
+      if (str.indexOf('c') > 0) J2.writeMicroseconds(n);
+      if (str.indexOf('d') > 0) J3.writeMicroseconds(n);
+      if (str.indexOf('e') > 0) J4.writeMicroseconds(n);
+      else {
+        Serial.println(str);
+      }
+      str = "";
+    }
+    else {
+      str += serialData;
+    }
+
+
+  } // end ifSerialAvailable
+
 }
